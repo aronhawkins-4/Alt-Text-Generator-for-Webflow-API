562f8a02600e0d4ad06e2c7827e24c742dd3a047
Aron Hawkins <hawkins.aron@gmail.com>
2024-02-13 16:18:19 -0600

    initial commit

--- index.js
+++ index.js
@@ -1,65 +1,104 @@
-const express = require('express');
-const cors = require('cors');
+import "dotenv/config";
+import express, { json as _json } from "express";
+import cors from "cors";
+import axios from "axios";
+import bops from "bops";
+import ConvertAPI from "convertapi";
+
+const convertapi = new ConvertAPI(process.env.CONVERTAPI_SECRET);
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(_json());
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000!');
+  console.log("Listening on port 3000!");
 });
 
-app.get('/', (req, res) => {
-  const {param1} = req.query;
-
-  res.send('Hello World!<br>Param1 = ' + param1);
+app.get("/", (req, res) => {
+  res.send(new Date().toISOString());
 });
 
-let nexPersonId = 3;
-const people = [
-  {id: 1, name: 'John', surname: 'Doe'},
-  {id: 2, name: 'Anna', surname: 'Dopey'},
-];
-
-app.get('/people', (req, res) => {
-  res.send(people);
-});
-
-app.get('/people/:id', (req, res) => {
-  const personId = +req.params.id;
-
-  const person = people.find(person => person.id === personId);
-
-  if(!person) {
-    res.sendStatus(404);
+app.post("/generate", (req, res) => {
+  console.log(req.body);
+  if (!req.body) {
+    res.status(400).json({ error: "Body not specified" });
     return;
   }
+  if (!req.body.url) {
+    res.status(400).json({ error: "No URL specified" });
+    return;
+  }
+  if (!req.body.url.includes("http")) {
+    res.status(400).json({ error: "Invalid URL format" });
+    return;
+  }
+  const reqUrl = req.body.url;
 
-  res.send(person);
+  if (reqUrl.slice(-3) === "svg") {
+    convertSvg(reqUrl).then((response) => {
+      generate(response, res);
+    });
+  } else {
+    generate(reqUrl, res);
+  }
 });
 
-app.post('/people', (req, res) => {
-  if(!req.body){
-    res.status(400).json({ error: 'Body not specified' });
-    return;
-  }
+function generate(url, res) {
+  axios
+    .get(url, { responseType: "arraybuffer" })
+    .then((axiosRes) => {
+      const uintArr = new Uint8Array(axiosRes.data);
+      const regularArr = Array.from(uintArr);
+      const bopsArray = bops.from(regularArr, "utf-8");
+      return bopsArray;
+    })
+    .then((array) => {
+      const apiUrl =
+        "https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-base";
+      const apiKey = process.env.HUGGINGFACE_API_KEY;
+      console.log("apiKey: " + apiKey);
+      fetch(apiUrl, {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+        method: "POST",
+        body: array,
+      })
+        .then((res) => {
+          return res.json();
+        })
+        .then((json) => {
+          if (json && json[0].generated_text) {
+            res.send(json[0].generated_text);
+          } else {
+            res.status(400).json({ error: "Invalid response format" });
+          }
+        })
+        .catch((error) => {
+          console.log(error.message);
+          res.status(500).json({ error: "Server Error" });
+        });
+    });
+}
 
-  if(!req.body.name){
-    res.status(400).json({ error: 'No name specified' });
-    return;
-  }
-
-  if(!req.body.surname){
-    res.status(400).json({ error: 'No surname specified' });
-    return;
-  }
-
-  const newPerson = {
-    ...req.body,
-    id: nexPersonId++
-  };
-
-  people.push(newPerson);
-
-  res.send(newPerson);
-});
+async function convertSvg(url) {
+  const convertedUrl = await convertapi
+    .convert(
+      "png",
+      {
+        File: url,
+      },
+      "svg"
+    )
+    .then(function (result) {
+      if (result) {
+        const url = result.file.url;
+        if (url) {
+          return url;
+        }
+        return undefined;
+      }
+    });
+  return convertedUrl;
+}
